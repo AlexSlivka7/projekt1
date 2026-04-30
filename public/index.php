ce4c204a97f663ffabf145a47e0e4fa12824100b
Alex Slivka <slivkaalex7@gmail.com>
2026-04-30 12:58:28 +0200

    databaza

--- public/index.php
+++ public/index.php
@@ -1,6 +1,17 @@
 <?php
 
+require_once __DIR__."/../vendor/autoload.php";
+
 session_start();
 
-require_once __DIR__."/../vendor/autoload-php";
+use App\Core\Database;
+use App\Repositories\UserRepository;
+
+$db = new Database();
+
+$pdo = $db->spojenie();
+
+var_dump($pdo);
+
+$userRepo = new UserRepository($pdo);
 ?>
